d138dd30ed05b745b2785d846de811c88272e0ac
Huzaifa bi n Ahmed <huzaifamalik3216@gmail.com>
2023-01-25 21:19:44 +0500

    completed with typscript

---
+++ simplestorage-with-hardhat-typescript/scripts/deploy.ts
@@ -0,0 +1,36 @@
+import { ethers, run, network } from "hardhat";
+
+async function main() {
+  const contractfactory = await ethers.getContractFactory("SimpleStorage");
+  console.log("Deploying contract");
+  const simplestorage = await contractfactory.deploy();
+
+  console.log(`deployed at address = ${(await simplestorage).address}`);
+
+  if (network.config.chainId === 5 && process.env.etherscan_apikey) {
+    await simplestorage.deployTransaction.wait(6);
+    const address = (await simplestorage).address;
+    await verify(address, []);
+  }
+}
+async function verify(conractaddress: string, args: any[]) {
+  console.log("verifying Contract");
+  try {
+    await run("verify:verify", {
+      address: conractaddress,
+      constructorArguments: args,
+    });
+  } catch (e: any) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified");
+    } else {
+      console.log(e);
+    }
+  }
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
