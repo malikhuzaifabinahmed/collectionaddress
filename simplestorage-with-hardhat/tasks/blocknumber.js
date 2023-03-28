0fcbfb4b1b1a1564c6c52136ceec2a01db561b6e
malikhuzaifabinahmed <65243618+malikhuzaifabinahmed@users.noreply.github.com>
2023-03-28 19:53:39 +0500

    Delete simplestorage-with-hardhat directory

--- simplestorage-with-hardhat/tasks/blocknumber.js
+++
@@ -1,8 +0,0 @@
-const { task } = require("hardhat/config")
-
-task("block-number", "Prints the current block number").setAction(async () => {
-  const blocknumber = await hre.ethers.provider.getBlockNumber()
-  console.log(`Current Block number : ${blocknumber}`)
-})
-
-module.exports = {}
