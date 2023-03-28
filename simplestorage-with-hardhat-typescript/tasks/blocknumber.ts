59046aec0d54ca58afa780228c8d4d0e42e2eb15
malikhuzaifabinahmed <65243618+malikhuzaifabinahmed@users.noreply.github.com>
2023-03-28 19:53:27 +0500

    Delete simplestorage-with-hardhat-typescript directory

--- simplestorage-with-hardhat-typescript/tasks/blocknumber.ts
+++
@@ -1,9 +0,0 @@
-import { task } from "hardhat/config";
-
-export default task(
-  "block-number",
-  "Prints the current block number"
-).setAction(async () => {
-  const blocknumber = await hre.ethers.provider.getBlockNumber();
-  console.log(`Current Block number : ${blocknumber}`);
-});
